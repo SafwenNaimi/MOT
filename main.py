ffbaf794410cd32b37bbcb0972bb91d0719948f4
Safwen Naimi <safwen.naimi@gmail.com>
2022-02-17 11:55:48 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -8,8 +8,112 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
+import mediapipe as mp
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+
 import tensorflow as tf
 from yolov3_tf2.models import YoloV3
-from yolov3_tf2.dataset import transfrom_images
+from yolov3_tf2.dataset import transform_images
+from yolov3_tf2.utils import convert_boxes
+
+from deep_sort import preprocessing
+from deep_sort import nn_matching
+from deep_sort.detection import Detection
+from deep_sort.tracker import Tracker
+from tools import generate_detections as gdet
+
+class_names = [c.strip() for c in open('./data/labels/coco.names').readlines()]
+yolo = YoloV3(classes = len(class_names))
+yolo.load_weights('./weights/yolov3.tf')
+
+max_cosine_distance = 0.5
+nn_budget = None
+nms_max_overlap = 0.8
+
+model_filename = 'model_data/mars-small128.pb' #To track pedestrians
+encoder = gdet.create_box_encoder(model_filename, batch_size=1)
+metric = nn_matching.NearestNeighborDistanceMetric('cosine', max_cosine_distance, nn_budget)
+tracker = Tracker(metric)
 
 
+vid = cv2.VideoCapture('data/video/vid.mp4')
+
+codec = cv2.VideoWriter_fourcc(*'XVID')
+vid_fps = int(vid.get(cv2.CAP_PROP_FPS))
+vid_width, vid_height = int(vid.get(cv2.CAP_PROP_FRAME_WIDTH)), int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
+out = cv2.VideoWriter('./data/video/results.mp4', codec, vid_fps, (vid_width, vid_height))
+
+while True:
+    _, img = vid.read()
+    if img is None:
+        print('NOOOOOOOO')
+        break
+
+    img_in = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img_in = tf.expand_dims(img_in, 0)
+    img_in = transform_images(img_in, 416)
+
+    t1 = time
+
+    boxes, scores, classes, nums = yolo.predict(img_in)
+
+    classes = classes[0]
+    names = []
+    for i in range(len(classes)):
+        names.append(class_names[int(classes[i])])
+    names = np.array(names)
+    converted_boxes = convert_boxes(img, boxes[0])
+    features = encoder(img, converted_boxes)
+
+    detections = [Detection(bbox, score, class_name, feature) for bbox, score, class_name, feature in zip(converted_boxes, scores[0], names, features)]
+    
+    boxs= np.array([d.tlwh for d in detections])
+    scores = np.array([d.confidence for d in detections])
+    classes = np.array([d.class_name for d in detections])
+
+    indices = preprocessing.non_max_suppression(boxs, classes, nms_max_overlap, scores)
+    detections = [detections[i] for i in indices]
+    
+    tracker.predict()
+    tracker.update(detections)
+
+    cmap = plt.get_cmap('tab20b')
+    colors = [cmap(i)[:3] for i in np.linspace(0,1,20)]
+    
+    for track in tracker.tracks:
+        if not track.is_confirmed() or track.time_since_update >1:
+            continue
+        bbox = track.to_tlbr()
+        class_name = track.get_class()
+        color = colors[int(track.track_id) % len(colors)]
+        color = [i * 255 for i in color]
+        if class_name =='person':
+            with mp_pose.Pose(min_detection_confidence=0.8, min_tracking_confidence=0.8) as pose:
+                roi = img[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])]
+                image = cv2.cvtColor(roi, cv2.COLOR_BGR2RGB)
+                image.flags.writeable = False
+                results =pose.process(image)
+                image.flags.writeable = True
+                image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+                mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+                cv2.imshow(class_name+"-" + str(track.track_id),image)
+            cv2.rectangle(img, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), color, 2)
+            cv2.rectangle(img, (int(bbox[0]), int(bbox[1])-30), (int(bbox[0])+(len(class_name)+len(str(track.track_id)))*17, int(bbox[1])), color, -1)
+            cv2.putText(img, class_name+"-" + str(track.track_id), (int(bbox[0]), int(bbox[1])-10),0, 0.75, (255,255,255),2)
+        
+    #fps = 1./(time.time() - t1)
+    #cv2.putText(img, "FPS: {:.2f}".format(fps), (0,30), 0, 1, (0,0,255), 2)
+    #cv2.resizeWindow('output',256,256)
+    cv2.imshow('outt',img)
+    
+
+    out.write(img)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+vid.release()
+out.release()
+cv2.destroyAllWindows()
